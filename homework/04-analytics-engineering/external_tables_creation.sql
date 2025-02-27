1144f1a3e86cf262024b7660f485945bb553d4f5
Xincheng Luo <115096594+TheoDeann@users.noreply.github.com>
2025-02-27 00:45:22 +0000

    keep csv.gz format

--- homework/04-analytics-engineering/external_tables_creation.sql
+++ homework/04-analytics-engineering/external_tables_creation.sql
@@ -1,24 +1,28 @@
 -- green trip data
 CREATE OR REPLACE EXTERNAL TABLE `round-fold-449120-g9.trips_data_all.green_tripdata`
 OPTIONS (
-  format = "PARQUET",
-  uris = ["gs://dtc-data-lake-xcluo/green/*.parquet"]
+  format = "CSV",  
+  uris = ["gs://dtc-data-lake-xcluo/green/*.csv.gz"],
+  skip_leading_rows = 1
 );
 
 -- yellow trip data
 CREATE OR REPLACE EXTERNAL TABLE `round-fold-449120-g9.trips_data_all.yellow_tripdata`
 OPTIONS (
-  format = "PARQUET",
-  uris = ["gs://dtc-data-lake-xcluo/yellow/*.parquet"]
+  format = "CSV",  
+  uris = ["gs://dtc-data-lake-xcluo/yellow/*.csv.gz"],
+  skip_leading_rows = 1 
 );
 
 -- fhv trip data
 CREATE OR REPLACE EXTERNAL TABLE `round-fold-449120-g9.trips_data_all.fhv_tripdata`
 OPTIONS (
-  format = "PARQUET",
-  uris = ["gs://dtc-data-lake-xcluo/fhv/*.parquet"]
+  format = "CSV",  
+  uris = ["gs://dtc-data-lake-xcluo/fhv/*.csv.gz"],
+  skip_leading_rows = 1 
 );
 
-SELECT COUNT(*) AS row_count 
-FROM `round-fold-449120-g9.trips_data_all.fhv_tripdata`;
+
+
+
 
